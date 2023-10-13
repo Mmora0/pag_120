142aa548307734dd88abeb61319fcaf50d9e64b9
Miguel Angel Mora Ortiz <125917913+Mmora0@users.noreply.github.com>
2023-10-13 13:37:52 -0500

    Add files via upload

---
+++ Evaluación.cs
@@ -0,0 +1,39 @@
+﻿//var numero1 = 5;
+//var numero2 = 7;
+//Console.WriteLine("Por favor, ingresa la suma de " + numero1 + " y " + numero2 + " :");
+//var respuesta = Console.ReadLine();
+//var resultado = Convert.ToInt32(respuesta);
+//var esCorrecto = resultado == (numero1 + numero2);
+//Console.WriteLine("Tu respuesta es " + esCorrecto);
+
+
+//var numero = 7;
+//Console.WriteLine("Por favor, ingresa el doble de " + numero + ":");
+//var respuesta = Console.ReadLine();
+//var resultado = Convert.ToInt32(respuesta);
+//var esCorrecto = resultado == (numero * 2);
+//Console.WriteLine("Tu respuesta es " + esCorrecto);
+
+
+//var numero = 42;
+//Console.WriteLine("Por favor, ingresa el número " + numero + ":");
+//var respuesta = Console.ReadLine();
+//var numeroIngresado = Convert.ToInt32(respuesta);
+//var esCorrecto = numeroIngresado == numero;
+//Console.WriteLine("Tu respuesta es " + esCorrecto);
+
+
+//var numero = 10;
+//Console.WriteLine("El valor inicial es " + numero);
+//numero ++;
+//Console.WriteLine("Después de incrementar, el valor es " + numero);
+//numero --;
+//Console.WriteLine("Después de decrementar, el valor es " + numero);
+
+
+//const double PI = 3.14159265359;
+//double radio = 5.0;
+//double area = PI * radio * radio;
+//Console.WriteLine("El valor de PI es constante " + PI);
+//Console.WriteLine("Dado un radio de " + radio + " unidades.");
+//Console.WriteLine("El área de un círculo es: " + area + " unidades cuadradas");
